bdf720baaf46cfbae19316d4b90e22bfee50a9c2
ngodi <ngodinhtri@outlook.com>
2023-05-24 16:31:27 +0700

    [Quiz.js] - optimized the code

--- quiz js/main.js
+++ quiz js/main.js
@@ -1,7 +1,7 @@
 // Elements
 const progressLine = document.getElementById("progress-line");
-const currQuestionEl = document.getElementById("currentQuestion");
-const totalQuestionEl = document.getElementById("totalQuestion");
+const currentQuestionElement = document.getElementById("currentQuestion");
+const totalQuestionElement = document.getElementById("totalQuestion");
 const questionDiv = document.getElementById("question");
 const answersButtons = document.querySelectorAll("#answers button.btn");
 const explanationDiv = document.getElementById("explanation");
@@ -11,43 +11,60 @@ const nextQuestionButton = document.getElementById("next-btn");
 const questions = [
   {
     question: "Was JavaScript invented in 1995 ?",
-    answer: "true",
+    answer: true,
     explanation:
       "Brendan Eich created JS at Netscape in 1995. The initial version of the language was written in just 10 days.",
   },
   {
     question: "Are strings in JS editable values ?",
-    answer: "false",
+    answer: false,
     explanation:
       "In JavaScript strings are immutable values, meaning they cannot be edited; however, they can replaced with new, different strings.",
   },
   {
     question: "1 + 1 === 2",
-    answer: "true",
+    answer: true,
     explanation: "The plus operator gives the sum of two numbers.",
   },
   {
     question: "'1' + '1' === '2'",
-    answer: "false",
+    answer: false,
     explanation:
       "The plus operator concatenates (joins together) strings, so '1' + '1' === '11'.",
   },
   {
     question: "typeof ['J', 'S'] === 'array'",
-    answer: "false",
+    answer: false,
     explanation:
       "Arrays have the type 'object'. In JS, everything is either a primitive data type (e.g. 'string', 'number') or an object. Arrays are a kind of object with some special properties.",
   },
 ];
 
-// Actions with elements
+// Init
+const TOTAL_QUESTIONS = questions.length;
+let questionIndex = 0;
+let score = 0;
+
+function init() {
+  //The beginning
+  totalQuestionElement.textContent = TOTAL_QUESTIONS;
+  progressLine.style.width = 0;
+  update();
+  // Actions with the buttons
+  answersButtons.forEach((button) =>
+    button.addEventListener("click", handleClick)
+  );
+  nextQuestionButton.addEventListener("click", () => update());
+}
+
+init();
+
+// Actions with the elements
 function hide(element) {
-  element.classList.add("hidden");
   element.classList.remove("show");
 }
 function show(element) {
   element.classList.add("show");
-  element.classList.remove("hidden");
 }
 function disable(button) {
   button.setAttribute("disabled", "");
@@ -56,51 +73,55 @@ function enable(button) {
   button.removeAttribute("disabled");
 }
 
-let currentQuestion = 0;
-let score = 0;
-const total = questions.length;
+// Check the answer
+function isCorrect(answer) {
+  let a = String(answer);
+  let b = String(questions[questionIndex].answer);
 
-function refreshScreen() {
-  currQuestionEl.textContent = currentQuestion + 1;
-  questionDiv.textContent = questions[currentQuestion].question;
-
-  hide(explanationDiv);
-  disable(nextQuestionButton);
-
-  answersButtons.forEach((button) => enable(button));
+  return a === b;
 }
 
-function update(answer) {
-  explanationDiv.textContent = questions[currentQuestion].explanation;
-  show(explanationDiv);
+// Update screen function
+function update() {
+  //update the question
+  currentQuestionElement.textContent = questionIndex + 1;
+  questionDiv.textContent = questions[questionIndex].question;
+  hide(explanationDiv);
 
+  //update the buttons
+  answersButtons.forEach((button) => {
+    button.classList.remove("true", "false");
+    enable(button);
+  });
+  disable(nextQuestionButton);
+}
+
+//Handle click function
+function handleClick(event) {
+  //disable & enable the buttons
   answersButtons.forEach((button) => disable(button));
   enable(nextQuestionButton);
 
-  score = answer === questions[currentQuestion].answer ? score + 1 : score;
-  currentQuestion++;
+  //show the explanation
+  explanationDiv.textContent = questions[questionIndex].explanation;
+  show(explanationDiv);
 
-  progressLine.style.width = `${(currentQuestion * 100) / total}%`;
+  //update score & questionIndex
+  let isCorrectAnswer = isCorrect(
+    event.target.value,
+    questions[questionIndex].answer
+  );
+  score = isCorrectAnswer ? score + 1 : score;
+  questionIndex++;
+  event.target.classList.add(String(isCorrectAnswer));
 
-  if (currentQuestion === total) {
-    nextQuestionButton.textContent = `No more questions! Your score is ${score} ✨`;
+  //update the progress line
+  progressLine.style.width = `${(questionIndex * 100) / TOTAL_QUESTIONS}%`;
+
+  //finish the game
+  if (questionIndex === TOTAL_QUESTIONS) {
     disable(nextQuestionButton);
+    nextQuestionButton.textContent = `No more questions! You have answered ${score} 
+    ${score !== 1 ? "questions" : "question"} correctly ✨`;
   }
 }
-
-function init() {
-  totalQuestionEl.textContent = total;
-  progressLine.style.width = `${(currentQuestion * 100) / total}%`;
-
-  refreshScreen();
-
-  answersButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      update(e.target.value);
-    });
-  });
-
-  nextQuestionButton.addEventListener("click", (e) => refreshScreen());
-}
-
-init();
